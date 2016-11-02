c1a0b7bb28f2d4784356991aec3cf83ba9d53fc2
José Luis Valencia Herrera <pepedou@gmail.com>
2016-11-01 20:18:06 -0600

    Initial Commit

---
+++ CronopiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CronopiOS
+//
+//  Created by José Luis Valencia Herrera on 01/11/16.
+//  Copyright © 2016 José Luis Valencia Herrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
