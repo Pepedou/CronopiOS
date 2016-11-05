60ea209f416f2c563b6becc5dc7fadae2e83b9f6
José Luis Valencia Herrera <pepedou@gmail.com>
2016-11-04 20:32:13 -0600

    Added pageViewController.

---
+++ CronopiOS/PageViewController.swift
@@ -0,0 +1,71 @@
+//
+//  PageViewController.swift
+//  CronopiOS
+//
+//  Created by José Luis Valencia Herrera on 04/11/16.
+//  Copyright © 2016 José Luis Valencia Herrera. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
+    }
+    
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newViewController(identifier: "ImagePreviewController"),
+                self.newViewController(identifier:"TestController")]
+    }()
+    
+    private func newViewController(identifier: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: identifier)
+    }
+}
+
+extension PageViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+}
